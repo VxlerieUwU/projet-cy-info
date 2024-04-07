a8714cb67259aff87b2c21f48770ca400cdcede4
Pyrodaz <160551227+Pyrodaz@users.noreply.github.com>
2024-04-07 17:50:46 +0200

    Create main.c
    Création du générateur de la graine

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int creation_graine(){
+	/* L'utilisateur rentre un entier qui crée la graine qui sert à générer le donjon de manière procédurale. 
+	Si le nombre est invalide plus de 3 fois de suite c'est à dire qu'il n'est pas un entier ou est trop 
+ 	grand, la graine est choisie aléatoirement.*/
+	int graine;
+	int verif_int; //Sert à vérifier que la graine est bien un entier.
+	int compteur_tentatives = 0; //Compte le nombre de fois que le nombre de la graine est invalide.
+
+	srand(time(NULL));
+	
+	do{
+		printf("Entrez une graine pour générer le jeu.\n");
+		verif_int = scanf("%d",&graine);
+		compteur_tentatives++;
+	}while(graine<0 || verif_int != 1 || compteur_tentatives != 3);
+	if(compteur_tentatives == 3){
+		graine = rand();
+	}
+	return graine;
+}
+
+
+int main(){
+	int graine;
+	graine = creation_graine();
+	srand(graine);
+	
+	return 0;
+}
