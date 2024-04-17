fea58ad34a33a605d70b6cb57a3e7be4219dac0e
Pyrodaz <160551227+Pyrodaz@users.noreply.github.com>
2024-04-17 13:56:47 +0200

    Create generateur_salles_v2.h
    Même résultat que generateur_salles mais cette fois la salle est mis dans un tableau 2D au lieu d'être affichée directement dans le terminal

---
+++ generateur_salles_v2.h
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <wchar.h>
+#include <locale.h>
+#define TAILLE_MAX 10
+
+
+int main(){
+    wchar_t** salle = NULL; //Création du pointeur salle
+    int taille_verticale, taille_horizontale;
+    srand(time(NULL));
+    setlocale(LC_CTYPE, ""); //Sélectionne la table unicode de la machine en locale.
+
+
+    taille_verticale = TAILLE_MAX-(rand()%(TAILLE_MAX-2));
+    taille_horizontale = TAILLE_MAX-(rand()%(TAILLE_MAX-2));
+
+    salle = malloc(sizeof(wchar_t*)*taille_verticale*taille_horizontale);
+    if(salle == NULL){
+        printf("Espace mémoire de la salle non alloué");
+        exit(1);
+        
+    }
+    for(int i=0;i<taille_verticale;i++){
+      //Allocation des lignes de la salle
+      
+      salle[i] = malloc(sizeof(wchar_t)*taille_horizontale);
+      //Vérification allocation
+      if(salle[i] == NULL){
+        printf("Espace mémoire de la ligne de la salle non alloué");
+        exit(2);
+        
+      }
+
+
+        
+      for(int j=0;j<taille_horizontale;j++){
+        salle[i][j] = ' ';
+            
+        if(i==0 || i==taille_verticale-1){
+            salle[i][j] = 0x02550; //Affichage bordure droite et gauche
+
+          }
+        else if(j==0 || j==taille_horizontale-1){
+          salle[i][j] = 0x02551; //Affichage bordure haut et bas
+
+        }   
+
+      }
+    }
+    //Bord supérieur gauche
+    salle[0][taille_horizontale-1] = 0x2557;
+
+    //Bord supérieur droit
+    salle[0][0]=0x2554;
+
+    //Bord inférieur gauche
+    salle[taille_verticale-1][taille_horizontale-1] = 0x255D;
+    //Bord inférieur droit
+    salle[taille_verticale-1][0] = 0x255A;
+
+
+  //Affichage salle
+  for(int i=0;i<taille_verticale;i++){
+    for(int j=0;j<taille_horizontale;j++){
+      printf("%lc",salle[i][j]);
+    }
+    printf("\n");
+  }
+
+    return 0;
+}
