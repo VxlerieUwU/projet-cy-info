4ff840b814d11e82d9d1d2c1e80e6ee1e21079b4
Florian <73788253+LeTaitre@users.noreply.github.com>
2024-05-08 22:56:56 +0200

    creation des collisions

--- interactions.c
+++ interactions.c
@@ -5,30 +5,38 @@ void interactions(int touche, Joueur* joueur, WINDOW* mainwin, char* logBuffer, 
 	switch(touche){
 			//change la position du joueur et efface le caractere qui se trouve a sa position actuelle
 			case KEY_UP:
-				mvwaddch(mainwin,joueur->y,joueur->x,' ');
-				joueur->y--;
+				if(mvwinch(mainwin, joueur->y-1, joueur->x)==' '){ //verifie que le caractere d a cote est un espace, pour les collisions
+					mvwaddch(mainwin,joueur->y,joueur->x,' ');
+					joueur->y--;
+				}
 				break;
 			case KEY_DOWN:
-				mvwaddch(mainwin,joueur->y,joueur->x,' ');
-				joueur->y++;
+				if(mvwinch(mainwin, joueur->y+1, joueur->x)==' '){
+					mvwaddch(mainwin,joueur->y,joueur->x,' ');
+					joueur->y++;
+				}
 				break;
 			case KEY_LEFT:
-				mvwaddch(mainwin,joueur->y,joueur->x,' ');
-				joueur->x--;
+				if(mvwinch(mainwin, joueur->y, joueur->x-1)==' '){
+					mvwaddch(mainwin,joueur->y,joueur->x,' ');
+					joueur->x--;
+				}
 				break;
 			case KEY_RIGHT:
-				mvwaddch(mainwin,joueur->y,joueur->x,' ');
-				joueur->x++;
+				if(mvwinch(mainwin, joueur->y, joueur->x+1)==' '){
+					mvwaddch(mainwin,joueur->y,joueur->x,' ');
+					joueur->x++;
+				}
 				break;
-            case KEY_BACKSPACE:
-                sprintf(logBuffer, "position joueur x = %d, y = %d", joueur->x, joueur->y);
-                logMessage(DEBUG, logBuffer);
+        		case KEY_BACKSPACE:
+                		sprintf(logBuffer, "position joueur x = %d, y = %d", joueur->x, joueur->y);
+                		logMessage(DEBUG, logBuffer);
 				clearBuf(logBuffer);
 				if(salle != NULL) {
 					libereSalle(salle);
 				}
 				salle = creerSalleProced(joueur->x, joueur->y);
-                break;
+                		break;
 			default:
 				break;
 	}
