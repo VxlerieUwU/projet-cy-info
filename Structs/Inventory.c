8c13fcd4f0a7fc9d7b8aef30b34bea8761a4f23d
Pyrodaz <160551227+Pyrodaz@users.noreply.github.com>
2024-05-08 12:34:15 +0200

    Delete Structs directory

--- Structs/Inventory.c
+++
@@ -1,5 +0,0 @@
-#define INV_SIZE 10 //taille de l'inventaire
-
-typedef struct{
-	Item ItTab[INV_SIZE]; //l'inventaire est un tableau d'objets (a definir)
-}Inventory;
--- Structs/Room.c
+++
@@ -1,8 +0,0 @@
-typedef struct{
-	int xO; //abscisse de l'origine
-	int yO; // ordonnée de l'origine
-	int length;
-	int height;
-	int discovered;
-	Door doors[4];
-}Room;
