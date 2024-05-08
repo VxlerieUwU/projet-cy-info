b935a2e0553f43b62364cd4e08fe95243221c965
Pyrodaz <160551227+Pyrodaz@users.noreply.github.com>
2024-05-08 12:16:33 +0200

    Update CosmicYonder.h
    Mis en ordre alphabétique + Ajouts des constantes et bibliothèques manquantes

--- CosmicYonder.h
+++ CosmicYonder.h
@@ -1,29 +1,58 @@
 //Bibliothèques
-#include <wchar.h>
 #include <linux/input-event-codes.h>
 #include <locale.h>
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <wchar.h>
 
 //Constantes
-#define GRAPHICS_H_
-#define JOUEUR_H_
+#define GRAPHIQUES_H_
 #define ESC 27
-#define INV_SIZE 10 
+#define INV_TAILLE 10 
+#define JOUEUR_H_
+#define MUR_VERTI_CHR "\u2550"
+#define MUR_HORIZ_CHR "\u2551"
+#define MUR_SUPG_CHR "\u2557"
+#define MUR_SUPD_CHR "\u2554"
+#define MUR_INFG_CHR "\u255D"
+#define MUR_INFD_CHR "\u255A"
+#define PORTE_CHR "\u25A2"
+#define UNICODE_STRING_LONG 10
+#define TAILLE_MAX_V 10
+#define TAILLE_MAX_H 20
 
 //Structures
+
+typedef struct{
+	Item ItTab[INV_TAILLE]; //l'inventaire est un tableau d'objets (a definir)
+}Inventaire;
+
+typedef struct {
+    char nom[255];
+    int x;
+    int y;
+    int compteur;
+    int id;
+}Item;
+
+typedef struct{
+	int pv;
+	int xp;
+	int niv;
+	Inventaire inventaire;
+	int x;
+	int y;
+}Joueur;
+
+
 typedef struct {
     int x;
     int y;
 }Porte;
-
-typedef struct{
-	int x;
-	int y;
-}Tuile;
 
 typedef struct {
     int longueur;
@@ -36,26 +65,10 @@ typedef struct {
     int nportes; //Nombres de portes de la salle
 }Salle;
 
-typedef struct {
-    char name[255];
-    int x;
-    int y;
-    int count;
-    int id;
-}Item;
-
 typedef struct{
-	Item ItTab[INV_SIZE]; //l'inventaire est un tableau d'objets (a definir)
-}Inventory;
-
-typedef struct{
-	int hp;
-	int xp;
-	int lvl;
-	Inventory inventory;
 	int x;
 	int y;
-}Joueur;
+}Tuile;
 
 //Enumération des élements possibles de la salle
 enum {
@@ -65,13 +78,13 @@ enum {
     MUR_INFG,
     MUR_HORIZ,
     MUR_VERTI,
-    VOID,
+    VIDE,
     PORTE,
 };
 
 //Fonctions
 Salle * creerSalle(int taille_horizontale, int taille_verticale, int x, int y, int nportes);
 Salle * creerSalleProced(int x, int y);
-void drawSalle(WINDOW * win, Salle * salle);
-void freeSalle(Salle * salle);
+void dessineSalle(WINDOW * win, Salle * salle);
+void libereSalle(Salle * salle);
 void initJoueur(Joueur* joueur);
