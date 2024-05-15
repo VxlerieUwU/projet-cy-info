448c998fa22d2ced2865a764739e0917b78a3d2e
Pyrodaz <160551227+Pyrodaz@users.noreply.github.com>
2024-05-15 12:45:42 +0200

    Update CosmicYonder.h
    Ajout de creation_graine()

--- CosmicYonder.h
+++ CosmicYonder.h
@@ -118,3 +118,5 @@ void initJoueur(Joueur* joueur);
 void interactions(int touche, Joueur* joueur, Salle ** carte, int salles_existantes, WINDOW* mainwin); 
 //initialise un ennemi à l'aide d'attributs passés en paramètre
 Ennemi initEnnemi(int x, int y, int pv, int att, int def);
+int creation_graine(); //Crée la graine de génération du jeu.
+
